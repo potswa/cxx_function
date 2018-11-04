47adb83bbd35028560dbc90499d5cbdae464fda3
David Krauss <david_work@me.com>
2018-11-04 20:39:58 +0100

    Better swap test.

--- test/swap.cpp
+++ test/swap.cpp
@@ -3,17 +3,30 @@
 
 using namespace cxx_function;
 
+int n = 0, c = 0;
+
 struct vf {
+    vf( int in )
+        : m{ in }
+        { ++ n; }
+    vf( vf const & o )
+        : m{ o.m }
+        { ++ n; ++ c; }
+    ~ vf() { -- n; }
+
     int operator () () { return m; }
     int m;
 };
 
 
 int main() {
-    function< int() > f = vf{ 3 }, g = vf{ 10 };
-    std::swap( f, g );
+    {
+        function< int() > f = vf{ 3 }, g = vf{ 10 };
+        std::swap( f, g );
 
-    assert ( f() == 10 );
-    assert ( g() == 3 );
+        assert ( f() == 10 );
+        assert ( g() == 3 );
+    }
+    assert ( n == 0 );
+    assert ( c == 2 );
 }
-
